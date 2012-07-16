fa275aba047339644149ba85a9d32f3859e45013
SitRepAdmin NYC <team@osrce.com>
2012-07-15 20:39:50 -0400

    removed themes dir, fixed desktop.html to only use necessiary libs and created loadDesktopFiles script to pull dojo libs and install them in libs dir.

---
+++ scripts/loadDesktopFiles.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir temp_files
+cd temp_files
+wget http://download.dojotoolkit.org/release-1.7.3/dojo-release-1.7.3.tar.gz
+tar -zxvf dojo-release-1.7.3.tar.gz
+mv dojo-release-1.7.3/* ../lib/.
+cd ..
+rm -rf temp_files
+
+
