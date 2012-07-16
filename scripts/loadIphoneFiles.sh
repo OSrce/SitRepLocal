43ca2d75e598c4e2c385b8fca2892ca8e8bd64b8
SitRepAdmin NYC <team@osrce.com>
2012-07-15 21:03:16 -0400

    added scripts for loading bb and android themes

--- scripts/loadIphoneFiles.sh
+++ scripts/loadIphoneFiles.sh
@@ -2,7 +2,8 @@
 
 mkdir temp_files
 cd temp_files
-wget http://download.dojotoolkit.org/release-1.7.3/dojo-release-1.7.3.tar.gz
+#wget http://download.dojotoolkit.org/release-1.7.3/dojo-release-1.7.3.tar.gz
+wget http://www.sitrep.org/downloads/dojo-release-1.7.3.tar.gz
 tar -zxvf dojo-release-1.7.3.tar.gz
 mv dojo-release-1.7.3/* ../../lib/.
 
