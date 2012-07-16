ca0a996ccc51695ecc069ad63625f5f8a0cb58cb
SitRepAdmin NYC <team@osrce.com>
2012-07-15 20:55:21 -0400

    fixed iphone config

---
+++ scripts/loadIphoneFiles.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir temp_files
+cd temp_files
+wget http://download.dojotoolkit.org/release-1.7.3/dojo-release-1.7.3.tar.gz
+tar -zxvf dojo-release-1.7.3.tar.gz
+mv dojo-release-1.7.3/* ../../lib/.
+
+wget http://www.sitrep.org/downloads/iphone_theme.tar.gz
+tar -zxvf iphone_theme.tar.gz
+mv iphone ../../lib/dijit/themes/.
+
+cd ..
+rm -rf temp_files
+
+
