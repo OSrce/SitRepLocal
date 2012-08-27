b70b7479c728a36d38ec028f6383d89f9eb46fef
SitRepAdmin NYC <team@osrce.com>
2012-08-27 18:58:33 -0400

    Built iphone version now only uses a dozen or so files...

--- docs/build_dojo_srd.sh
+++ docs/build_dojo_srd.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 SRHOME=..
-SRSRC=$SRHOME/lib/srd
-DESTDIR=$SRHOME/build
+SRSRC=$SRHOME/lib_src/srd
+DESTDIR=$SRHOME/lib
 
 rm -rf $DESTDIR/dojo
 rm -rf $DESTDIR/dijit
 rm -rf $DESTDIR/dojox
 rm -rf $DESTDIR/srd
 
-$SRHOME/lib/util/buildscripts/build.sh --require "$SRSRC/run.js" --profile "$SRSRC/srd.profile.js" --releaseDir "$DESTDIR"
+$SRHOME/lib_src/util/buildscripts/build.sh --require "$SRSRC/run.js" --profile "$SRSRC/srd.profile.js" --releaseDir "$DESTDIR"
 #$SRHOME/lib/util/buildscripts/build.sh --require "$SRSRC/login_bootloader_iphone.js" --profile "$SRSRC/srd.profile.js" --releaseDir "$DESTDIR"
 #$SRHOME/lib/util/buildscripts/build.sh  --profile "$SRSRC/srd.profile.js" --releaseDir "$DESTDIR"
 
